e58a0a1fe3245bd6560b24fa6dd0ee267b57b053
Alvaro Poveda <alvaropoveda@ingenieros.com>
2022-06-18 12:59:10 -0500

    pthexasolutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pthexasolutions';
+  longitud: number = 0;
+  arrayRandom: number[] = [];
+  arraySort: number[] = [];
+  generarArray() {
+    this.arrayRandom = Array.from({ length: this.longitud }, () =>
+      Math.floor(Math.random() * 100)
+    );
+    this.arraySort = this.arrayRandom.slice();
+    this.arraySort.sort((a, b) => a - b);  
+  }
 }
